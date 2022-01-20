b89ac729f4b0c0806f18575404192b9230831916
ryoko-y <wagarden6@gmail.com>
2022-01-20 18:57:06 -0500

    Fixed sidebar link issue

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
+const sidebarLinks = document.querySelector(".sidebar-links");
 const date = document.querySelector("#date");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -11,12 +13,17 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-fixed");
   }
 });
-// show sidebar
+
+// toggle sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+sidebarLinks.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
 // set year
 date.innerHTML = new Date().getFullYear();
